a03aea7d77f7193452b34d3e1e0062ff67ca6dc8
MaryPV <fornewstudies@gmail.com>
2025-09-27 09:53:51 +0500

    Initial commit

---
+++ src/main/java/org/skypro/Cource2/ExaminerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.Cource2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminerServiceApplication.class, args);
+	}
+
+}
